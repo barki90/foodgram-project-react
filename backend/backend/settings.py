d5cdf695b7e8b06952ae8cabab1707d2fe864cc7
Vladimir Berezin <barclays90@mail.ru>
2022-11-21 23:06:54 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = 'django-insecure-(1otmk)3zc@9rjqvd88sx&(egh-r55ru7e=#k!!zp!*+((uku7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['84.201.174.166', '127.0.0.1', 'localhost']
 
 # Application definition
 
